3d12ea6fbb319cc503b11e4d4ed79f80cc561e4f
Aaron Denney <wnoise@ofb.net>
2015-05-05 17:31:09 -0700

    Sample red/green gradient with blue pulse.

--- main.cpp
+++ main.cpp
@@ -2,4 +2,17 @@
 
 int main()
 {
+    set_pin_modes();
+    start_timer();
+    unsigned count;
+    while (1) {
+        for (unsigned i = 0; i < f.ROWS; ++i)
+        {
+            for (unsigned j = 0; j < f.COLS; ++j)
+            {
+                f.grid[i][j] = g35_pixel {200U, count%16, i, j};
+                ++count;
+            }
+        }
+    }
 }
